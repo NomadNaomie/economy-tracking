b08917df5b45e1596c6ba6dd2c0e72bfb2954611
NomadNaomie <nomadnaomie@protonmail.com>
2021-11-21 20:00:35 -0500

    Fixed and Zoomable Tree map

--- assets/js/treemap.js
+++ assets/js/treemap.js
@@ -9,7 +9,8 @@
 const tree_margin = {top: 10, right: 10, bottom: 10, left: 10},
   tree_width = 700 - tree_margin.left - tree_margin.right,
   tree_height = 850 - tree_margin.top - tree_margin.bottom;
-
+//Whether we are in the top level of not.
+var treemode = false;
 //Grabs the div with the id of treemap and creates a new SVG inside of it
 const tree_svg = d3.select("#treemap")
 .append("svg")
@@ -25,17 +26,22 @@ d3.json("/assets/data/treemap.json").then(function(data) {
   *   We create a hierarchy D3 object to hold the summation of each category, for now each category is singular so is its own sum in the data 
   *   
   */
-  const root = d3.hierarchy(data).sum(function(d){ return d.value}) // Here the size of each leave is given in the 'value' field in input data
-
+  //We won't display the children until called.
+  const topLevelLeaves = []
+  for (const {children: _, ...o} of data.children) {
+    topLevelLeaves.push(o)
+  }
+  const root = d3.hierarchy({children:topLevelLeaves}).sum(function(d){console.log(d.value);return d.value}) // Here the size of each leaf is given in the 'value' field in input data
+  console.log(root)
   // Then d3.treemap computes the position of each element of the hierarchy
-  d3.treemap()
+  let treemapObject = d3.treemap()
     .size([tree_width, tree_height])
     .padding(3)
     (root)
 
   //Using the d3.treemap object we can create the rectangles for the leaves
   //Color is currently blue determined by the remainder of the transaction / 200
-  tree_svg
+  let leaves = tree_svg
     .selectAll("rect")
     .data(root.leaves())
     .join("rect")
@@ -43,9 +49,10 @@ d3.json("/assets/data/treemap.json").then(function(data) {
       .attr('y', function (d) { return d.y0; })
       .attr('width', function (d) { return d.x1 - d.x0 + 10; })
       .attr('height', function (d) { return d.y1 - d.y0 + 10; })
+      .attr("district", function(d) {return d.data.name })
       .attr('id','tree-rect')
       .style("stroke", "black")
-      .style("fill", function(d) { return "rgb(0, 0, " + (d.data.value % 200) + ")"; })
+      .style("fill", function(d) { return "rgb(0, 0, " + (d.data.value % 2500) + ")"; })
 
   //Adding labels to each leaf rect with the category and number of diamonds
   tree_svg
@@ -57,4 +64,84 @@ d3.json("/assets/data/treemap.json").then(function(data) {
       .text(function(d){ return d.data.name + " - " + d.data.value})
       .attr("font-size", "15px")
       .attr("fill", "white")
+
+  //When clicked transform the leaves into their children
+  tree_svg
+    .data(root.leaves())
+    .on("click", function(d){
+      if(treemode){
+        treemode=false;
+        d3.treemap()
+        .size([tree_width, tree_height])
+        .padding(3)
+        (root)
+      let newLeaves = tree_svg
+        .selectAll("rect")
+        .data(root.leaves())
+        .join("rect")
+        .transition()
+        .duration(1000)
+          .attr('x', function (d) { return d.x0; })
+          .attr('y', function (d) { return d.y0; })
+          .attr('width', function (d) { return d.x1 - d.x0 + 10; })
+          .attr('height', function (d) { return d.y1 - d.y0 + 10; })
+          .attr("district", function(d) {return d.data.name })
+          .attr('id','tree-rect')
+          .style("stroke", "black")
+          .style("fill", function(d) { return "rgb(0, 0, " + (d.data.value % 2500) + ")"; })
+
+      tree_svg
+        .selectAll("text")
+        .data(root.leaves())
+        .join("text")
+        .transition()
+        .duration(1000)
+          .attr("x", function(d){ return d.x0+5})
+          .attr("y", function(d){ return d.y0+20})
+          .text(function(d){ return d.data.name + " - " + d.data.value})
+          .attr("font-size", "15px")
+          .attr("fill", "white")
+          .transition()
+          .duration(1000)
+      }
+      else{
+        treemode=true;
+      let filterkey = d.target.attributes.district.value;
+      console.log(filterkey)
+      newData = data.children.filter(function(d){return d.name == filterkey})
+      let newRoot = d3.hierarchy({children:newData[0].children}).sum(function(d){console.log(d);return d.value})
+      let newTreemapObject = d3.treemap()
+        .size([tree_width, tree_height])
+        .padding(3)
+        (newRoot)
+      let newLeaves = tree_svg
+        .selectAll("rect")
+        .data(newRoot.leaves())
+        .join("rect")
+        .transition()
+        .duration(1000)
+          .attr('x', function (d) { return d.x0; })
+          .attr('y', function (d) { return d.y0; })
+          .attr('width', function (d) { return d.x1 - d.x0 + 10; })
+          .attr('height', function (d) { return d.y1 - d.y0 + 10; })
+          .attr("district", function(d) {return d.data.name })
+          .attr('id','tree-rect')
+          .style("stroke", "black")
+          .style("fill", function(d) { return "rgb(0, 0, " + (d.data.value % 2500) + ")"; })
+
+      tree_svg
+        .selectAll("text")
+        .data(newRoot.leaves())
+        .join("text")
+        .transition()
+        .duration(1000)
+          .attr("x", function(d){ return d.x0+5})
+          .attr("y", function(d){ return d.y0+20})
+          .text(function(d){ return d.data.name + " - " + d.data.value})
+          .attr("font-size", "15px")
+          .attr("fill", "white")
+          .transition()
+          .duration(1000)
+      }
+  })
 })
